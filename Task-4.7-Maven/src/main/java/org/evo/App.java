c3784a9983c1fee656f50d64794001f66293fb84
lekuim <zabirovivan@Outlook.com>
2024-07-03 19:35:19 +0300

    4.7-fix

--- Task-4.7-Maven/src/main/java/org/evo/App.java
+++ Task-4.7-Maven/src/main/java/org/evo/App.java
@@ -1,6 +1,9 @@
 package org.evo;
 
-public class Main {
+/**
+ * Hello world!
+ */
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
     }
